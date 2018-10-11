6e3e0ea17b462f99aa4acab1ef0e7aa658a9b917
let12111 <let12111@mail.ru>
2018-10-11 14:10:59 +0300

    first commit

---
+++ Contracts/SDO/ACLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    class ACLog
+    {
+        int LogId { get; set; }
+        int PersID { get; set; }
+        string PersonName { get; set; }
+        string EventName { get; set; }
+        DateTime LogDate { get; set; }
+    }
+}
---
+++ Contracts/SDO/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.SDO
+{
+    class Person
+    {
+        int PersonId { get; set; }
+        string PersonName { get; set; }
+    }
+}
